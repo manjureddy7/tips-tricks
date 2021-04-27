74b8f7d019af5de6394978244f2e607ae236d443
Manoj Reddy <manureddy.g94@gmail.com>
2021-04-27 13:17:31 +0530

    Create Table.js

---
+++ Table.js
@@ -0,0 +1,5 @@
+---- HOW TO CREATE CUSTOM TABLE COMPONENT WITH LOT OF FLEXIBILITY ----
+
+Expectation: I want to create a Table component with just not only data, the table cell can be editable, can have buttons, Table header may be have dynamic content
+
+Procedure:
